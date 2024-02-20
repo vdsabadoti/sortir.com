824186590c8eb2d2d5ee3f50b2dba6b1d3477111
vivisabadoti <viviane.deroldosabadoti2023@campus-eni.fr>
2024-02-20 09:02:22 +0100

    Developping the CRUD of Site and Ville

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Site;
+use App\Form\SiteType;
+use App\Repository\SiteRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/site')]
+class SiteController extends AbstractController
+{
+    #[Route('/', name: 'app_site_index', methods: ['GET'])]
+    public function index(SiteRepository $siteRepository): Response
+    {
+        return $this->render('site/index.html.twig', [
+            'sites' => $siteRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_site_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $site = new Site();
+        $form = $this->createForm(SiteType::class, $site);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($site);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_site_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('site/new.html.twig', [
+            'site' => $site,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_site_show', methods: ['GET'])]
+    public function show(Site $site): Response
+    {
+        return $this->render('site/show.html.twig', [
+            'site' => $site,
+        ]);
+    }
+
+    #[Route('/filter', name: 'app_site_filter', methods: ['pOST'])]
+    public function showFilteredSites(Request $request, SiteRepository $siteRepository): Response
+    {
+        $text = $request->query->get('site');
+        dd($text);
+        $site = $siteRepository->findByText($text);
+
+        return $this->render('site/index.html.twig', [
+            'site' => $site,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_site_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Site $site, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(SiteType::class, $site);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_site_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('site/edit.html.twig', [
+            'site' => $site,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_site_delete', methods: ['POST'])]
+    public function delete(Request $request, Site $site, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$site->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($site);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_site_index', [], Response::HTTP_SEE_OTHER);
+    }
+}

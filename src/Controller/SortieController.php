72ff03bc5783faad3c1a85f4a0ceee4c639cb7d1
dbellaire2023 <bellaire.dorian@gmail.com>
2024-02-20 15:57:23 +0100

    Création de sortie testée

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
-use App\Form\RegistrationFormType;
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use App\Repository\EtatRepository;
 use App\Repository\ParticipantRepository;
 use App\Repository\SiteRepository;
 use App\Repository\SortieRepository;
@@ -75,6 +77,50 @@ class SortieController extends AbstractController
             'sorties' => $sorties,
             'sites' => $sites,
         ]);
+    }
+
+    #[Route('/sortie/creer', name:'app_sortie_create')]
+    public function creerSortie(EntityManagerInterface $em, Request $request, ParticipantRepository $p, EtatRepository $e) : Response
+    {
+
+        $sortie = new Sortie();
+
+        $form = $this->createForm(SortieType::class, $sortie);
+
+        $form->handleRequest($request);
+
+
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+
+            $em->beginTransaction();
+            try {
+                //$this->getUser()->getId();
+                $sortie->setOrganisateur($p->find(1));
+                $sortie->setEtat($e->find(1));
+                $em->persist($sortie->getLieu());
+                $em->flush();
+                $em->persist($sortie);
+                $em->flush();
+
+                $em->commit();
+            } catch (\Exception $e) {
+                $em->rollBack();
+                // Gérez l'exception
+            }
+
+
+
+            return $this->redirectToRoute('app_home');
+        }
+
+
+        return $this->render('sortie/creer.html.twig', [
+            'form' => $form
+        ]);
+
     }
 
 }

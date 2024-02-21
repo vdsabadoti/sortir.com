753833831f86dbcc6f6b834af3978e9a87e2a225
dbellaire2023 <bellaire.dorian@gmail.com>
2024-02-21 16:25:33 +0100

    Création entièrement testée ( il manque juste le css des erreurs )

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -8,6 +8,7 @@ use App\Entity\Participant;
 use App\Entity\Site;
 use App\Entity\Sortie;
 use App\Entity\Ville;
+use App\Repository\LieuRepository;
 use App\Repository\VilleRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
@@ -49,21 +50,59 @@ class SortieType extends AbstractType
                 'row_attr' => ['class' => 'my-1 flex flex-col'],
                 'attr' => ['class' => 'border-2 border-slate-400 ml-2 rounded-lg']
             ])
-            ->add('lieu', LieuType::class, [
-                'row_attr' => ['class' => 'my-1 flex flex-col'],
-                'attr' => ['class' => 'border-2 border-slate-400 ml-2 px-5 rounded-lg']
-            ])
-            ->add('submit', SubmitType::class, [
+            ->add('AjouterLieu', SubmitType::class, [
+                'label' => 'Ajouter un lieu manuelement',
+                'row_attr' => ['class' => 'w-fit mx-auto  flex flex-col'],
+                'attr' => ['class' => 'border-2 border-slate-400 mt-6 rounded-lg',
+                    'formnovalidate' => 'formnovalidate']
+
+            ]);
+            if ($options['addLieu']) { // ajoute un formulaire de création de lieu si l'utilisateur clique sur le bouton d'ajout de lieu
+
+                $builder->add('SelectionnerLieuxDisponibles', SubmitType::class, [
+                    'label' => 'Sélectionner parmis les lieux disponibles',
+                    'row_attr' => ['class' => 'w-fit mx-auto  flex flex-col'],
+                    'attr' => ['class' => 'border-2 border-slate-400 mt-6 rounded-lg',
+                        'formnovalidate' => 'formnovalidate']
+                ]);
+
+                $builder->add('lieu', LieuType::class, [
+                    'row_attr' => ['class' => 'my-1 flex flex-col'],
+                    'attr' => ['class' => 'border-2 border-slate-400 ml-2 px-5 rounded-lg']
+                ]);
+
+                $builder->remove('AjouterLieu');
+
+
+            } else
+            {
+                $builder->add('lieu', EntityType::class, [
+                    'class' => Lieu::class,
+                    'choice_label' => 'nom',
+                    'query_builder' => function(LieuRepository $lieuRepository) {
+                        return $lieuRepository->createQueryBuilder('l');
+                    },
+                    'row_attr' => ['class' => 'my-1 flex flex-col'],
+                    'attr' => ['class' => 'border-2 border-slate-400 mt-6 rounded-lg']
+                ]);
+
+            }
+
+            $builder->add('submit', SubmitType::class, [
                 'row_attr' => ['class' => 'w-fit mx-auto  flex flex-col'],
                 'attr' => ['class' => 'border-2 border-slate-400 mt-6 rounded-lg']
             ])
         ;
+
+
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Sortie::class,
+            'addLieu' => false,
         ]);
     }
 }

5e1daf773bc0372e67fe2c474698a3d4c309135e
vivisabadoti <viviane.deroldosabadoti2023@campus-eni.fr>
2024-02-21 16:04:41 +0100

    S'inscrire et desister des sorties

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Sortie;
 use App\Repository\EtatRepository;
 use App\Repository\ParticipantRepository;
+use App\Services\SortiesService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,19 +20,9 @@ class InscriptionController extends AbstractController
     }
 
     #[Route('/inscribe/{id}', name: 'app_inscribe', requirements: ['id' => '\d+'], defaults: ['id' => 1])]
-    public function inscribe(Sortie $sortie, EntityManagerInterface $em, ParticipantRepository $participantRepository, EtatRepository $etatRepository): Response
+    public function inscribe(Sortie $sortie, SortiesService $sortiesService): Response
     {
-        //TODO doublons
-        //Ajout du participant à la sortie si elle est ouverte (2)
-        if ($sortie->getEtat()->getId() === 2){
-            $sortie->addParticipant($participantRepository->findOneBy(['email' => $this->getUser()->getUserIdentifier()]));
-            //MAJ de l'état de la sortie si nb de participants atteint (ouverte (2) => cloturé(3))
-            if (count($sortie->getParticipants()) == $sortie->getNbInscriptionsMax()){
-                $sortie->setEtat($etatRepository->find(3));
-            }
-            //UPDATE DB
-            $em->persist($sortie);
-            $em->flush();
+        if ($sortiesService->ajouterParticipant($sortie, $this->getUser()->getUserIdentifier())){
             $this->addFlash('success', 'Inscription réalisé');
         } else {
             $this->addFlash('warning', 'Inscription impossible');
@@ -40,17 +31,9 @@ class InscriptionController extends AbstractController
     }
 
     #[Route('/desisted/{id}', name: 'app_desisted', requirements: ['id' => '\d+'], defaults: ['id' => 0])]
-    public function desisted(Sortie $sortie, EntityManagerInterface $em, ParticipantRepository $participantRepository, EtatRepository $etatRepository): Response
+    public function desisted(Sortie $sortie, SortiesService $sortiesService): Response
     {
-
-        //Deistement du participant à la sortie si elle est ouverte (2) ou cloturée (3)
-        if ($sortie->getEtat()->getId() === 2 || $sortie->getEtat()->getId() === 3){
-            $sortie->removeParticipant($participantRepository->find($this->getUser()->getUserIdentifier()));
-            //MAJ de l'état de la sortie
-            $sortie->setEtat($etatRepository->find(2));
-            //UPDATE DB
-            $em->persist($sortie);
-            $em->flush();
+        if ($sortiesService->retirerParticipant($sortie, $this->getUser()->getUserIdentifier())){
             $this->addFlash('success', 'Desistement pris en compte');
         } else {
             $this->addFlash('warning', 'Desistement impossible');
---
+++ src/Services/SortiesService.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Participant;
+use App\Entity\Sortie;
+use App\Repository\EtatRepository;
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class SortiesService
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+        private readonly EtatRepository $etatRepository,
+        private readonly ParticipantRepository $participantRepository)
+    {
+
+    }
+
+    //Ajouter un participant à la sortie (ouverte)
+    public function ajouterParticipant(Sortie $sortie, string $identifiant) : bool
+    {
+        $participant = $this->participantRepository->findOneBy(['email' => $identifiant]);
+
+        if ($this->sortieOuverte($sortie) && !$this->participantInscrit($sortie, $participant) && !$this->estOrganisateur($sortie, $participant)) {
+            $sortie->addParticipant($participant);
+            $this->majEtatSortie($sortie);
+            $this->majSortieBaseDonnees($sortie);
+            return true;
+        }
+        return false;
+    }
+
+    //Retirer un participant d'une sortie (ouverte ou cloturée)
+    public function retirerParticipant(Sortie $sortie, string $identifiant) : bool
+    {
+        $participant = $this->participantRepository->findOneBy(['email' => $identifiant]);
+
+        if ($this->participantInscrit($sortie, $participant) && $this->sortieOuverte($sortie) || $this->sortieCloturee($sortie)) {
+            $sortie->removeParticipant($participant);
+            $this->majEtatSortie($sortie);
+            $this->majSortieBaseDonnees($sortie);
+            return true;
+        }
+        return false;
+    }
+
+
+    //Verifie si la sortie est ouverte
+    private function sortieOuverte(Sortie $sortie) : bool {
+        return ($sortie->getEtat()->getId() === 2);
+    }
+
+    //Verifie si la sortie est cloturée
+    private function sortieCloturee(Sortie $sortie) : bool {
+        return ($sortie->getEtat()->getId() === 3);
+    }
+
+
+    //Verifie si un participant est inscrit dans un sortie
+    private function participantInscrit(Sortie $sortie, Participant $participant) : bool {
+        $participants = $sortie->getParticipants();
+        foreach ($participants as $p){
+            if ($p->getId() == $participant->getId()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Mise à jour de l'état d'une sortie
+    private function majEtatSortie(Sortie $sortie) : void {
+
+        //Cloturé si nb de participants atteint, sinon ouverte
+        if (count($sortie->getParticipants()) == $sortie->getNbInscriptionsMax()) {
+            $sortie->setEtat($this->etatRepository->findOneBy(['id'=> 3]));
+        } else {
+            $sortie->setEtat($this->etatRepository->findOneBy(['id'=> 2]));
+        }
+    }
+
+    //Mise à jour de la base de données
+    private function majSortieBaseDonnees(Sortie $sortie){
+        $this->em->persist($sortie);
+        $this->em->flush();
+    }
+
+    private function estOrganisateur(Sortie $sortie, Participant $participant) : bool {
+        return ($sortie->getOrganisateur()->getId() == $participant->getId());
+    }
+
+}

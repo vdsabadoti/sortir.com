5f046920b304b0173fc7060790d70df9c064dc9c
dbellaire2023 <bellaire.dorian@gmail.com>
2024-02-22 16:02:48 +0100

    Création et Modification entièrement fonctionnelle ( il manque juste le css des erreurs )

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -18,31 +18,38 @@ class SortieController extends AbstractController
 {
 
     #[Route('/sortie/create', name:'app_sortie_create')]
-    public function create(EntityManagerInterface $em, Request $request, ParticipantRepository $p, EtatRepository $e, SiteRepository $s) : Response
+    public function create(EntityManagerInterface $em, Request $request, EtatRepository $e) : Response
     {
 
         $sortie = new Sortie();
+        $user = $this->getUser();
 
-        $form = $this->createForm(SortieType::class, $sortie, ['addLieu' => isset($request->get('sortie')['AjouterLieu'])]);
+        $form = $this->createForm(SortieType::class, $sortie, [
+            'addLieu' => isset($request->get('sortie')['AjouterLieu']),
+            'InsererLieu' => isset($request->get('sortie')['InsererLieu'])
+        ]);
 
-        $sortie->setOrganisateur($p->find(1));
+        $sortie->setOrganisateur($user);
 
         $form->handleRequest($request);
 
-        //dd($request);
+        //dd($form);
+
+        if(isset($request->get('sortie')['AjouterLieu']))
+        {
+            return $this->render('sortie/creer.html.twig', [
+                'form' => $form
+            ]);
+        }
 
         if($form->isSubmitted() && $form->isValid())
         {
 
-
-
             $em->beginTransaction();
             try {
 
-
-               // $sortie->setOrganisateur($this->getUser()->getId()) ;
                 $sortie->setEtat($e->find(1));
-                $sortie->setSite($s->find(1));
+                $sortie->setSite($sortie->getOrganisateur()->getSite());
 
                 // On persiste le lieu
                 $em->persist($sortie->getLieu());
@@ -55,6 +62,9 @@ class SortieController extends AbstractController
                 $em->commit();
             } catch (\Exception $e) {
                 $em->rollBack();
+                //var_dump($sortie);
+                var_dump($e->getMessage());
+                //dd($e->getMessage());
                 return $this->render('sortie/creer.html.twig', [
                     'form' => $form
                 ]);
@@ -70,12 +80,81 @@ class SortieController extends AbstractController
 
     }
 
-//    #[Route('/sortie/update', name:'app_sortie_update')]
-//    public function update(EntityManagerInterface $em, Request $request) : Response
-//    {
-//
-//
-//
-//    }
+    #[Route('/sortie/update/{id}', name:'app_sortie_update')]
+    public function update(EntityManagerInterface $em, Request $request, Sortie $sortie) : Response
+    {
+
+        /* Si l'utilisateur veut ajouter un lieu on lui mets le formulaire de creation de lieu
+           tout en supprimant de l'objet transféré le lieu selectionner afin que les champs du nouveau formulaire soient vides
+        */
+        if(isset($request->get('sortie')['AjouterLieu']))
+        {
+
+            $form = $this->createForm(SortieType::class, $sortie, [
+                'addLieu' => isset($request->get('sortie')['AjouterLieu']),
+                'InsererLieu' => isset($request->get('sortie')['InsererLieu'])
+            ]);
+
+            $form->get('lieu')->setData(null);
+
+            $form->handleRequest($request);
+
+            return $this->render('sortie/creer.html.twig', [
+                'form' => $form
+            ]);
+        }
+
+
+        $form = $this->createForm(SortieType::class, $sortie, [
+            'addLieu' => isset($request->get('sortie')['AjouterLieu']),
+            'InsererLieu' => isset($request->get('sortie')['InsererLieu'])
+        ]);
+
+        // On resupprime le lieu de la liste déroulante afin de laisser place au nouveau afin de faire une insertion
+        if(isset($request->get('sortie')['InsererLieu']))
+        {
+            $form->get('lieu')->setData(null);
+        }
+
+        $form->handleRequest($request);
+
+
+        // Si l'utilisateur veut ajouter un lieu on lui renvoie le meme formulaire mais avec le lieux vide
+
+
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em->beginTransaction();
+            try {
+
+                //dd($sortie->getLieu());
+                // On persiste le lieu
+                $em->persist($sortie->getLieu());
+                $em->flush();
+
+                // On persiste la sortie
+                $em->persist($sortie);
+                $em->flush();
+
+                $em->commit();
+            } catch (\Exception $e) {
+                $em->rollBack();
+                var_dump($e->getMessage());
+                return $this->render('sortie/update.html.twig', [
+                    'form' => $form
+                ]);
+            }
+
+            return $this->redirectToRoute('app_sorties_par_sites', ['site' => $this->getUser()->getSite()->getNom()]);
+        }
+
+
+        return $this->render('sortie/update.html.twig', [
+            'form' => $form
+        ]);
+
+
+    }
 
 }

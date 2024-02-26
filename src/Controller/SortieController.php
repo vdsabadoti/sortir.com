c9394a43276980ec14517de2842e9ba12ce86e8c
dbellaire2023 <bellaire.dorian@gmail.com>
2024-02-26 11:33:47 +0100

    Ajout du css

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -8,14 +8,205 @@ use App\Repository\EtatRepository;
 use App\Repository\ParticipantRepository;
 use App\Repository\SiteRepository;
 use App\Repository\SortieRepository;
+use App\Services\SortiesService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class SortieController extends AbstractController
 {
+
+    #[Route('/sortie/create', name:'app_sortie_create')]
+    public function create(EntityManagerInterface $em, Request $request, EtatRepository $e) : Response
+    {
+
+        $sortie = new Sortie();
+        $user = $this->getUser();
+
+        $form = $this->createForm(SortieType::class, $sortie, [
+            'addLieu' => isset($request->get('sortie')['AjouterLieu']),
+            'InsererLieu' => isset($request->get('sortie')['InsererLieu'])
+        ]);
+
+        $sortie->setOrganisateur($user);
+
+        $form->handleRequest($request);
+
+        //dd($form);
+
+        if(isset($request->get('sortie')['AjouterLieu']))
+        {
+            return $this->render('sortie/creer.html.twig', [
+                'form' => $form
+            ]);
+        }
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+            $em->beginTransaction();
+            try {
+
+                $sortie->setEtat($e->find(1));
+                $sortie->setSite($sortie->getOrganisateur()->getSite());
+
+                // On persiste le lieu
+                $em->persist($sortie->getLieu());
+                $em->flush();
+
+                // On persiste la sortie
+                $em->persist($sortie);
+                $em->flush();
+
+                $em->commit();
+            } catch (\Exception $e) {
+                $em->rollBack();
+                //var_dump($sortie);
+                var_dump($e->getMessage());
+                //dd($e->getMessage());
+                return $this->render('sortie/creer.html.twig', [
+                    'form' => $form
+                ]);
+            }
+
+            return $this->redirectToRoute('app_sorties_par_sites', ['site' => $this->getUser()->getSite()->getNom()]);
+        }
+
+
+        return $this->render('sortie/creer.html.twig', [
+            'form' => $form
+        ]);
+
+    }
+
+    #[Route('/sortie/update/{id}', name:'app_sortie_update', requirements: ['id' => '\d+'])]
+    public function update(EntityManagerInterface $em, Request $request, Sortie $sortie, SortiesService $sortiesService) : Response
+    {
+
+        if($sortiesService->verifModificationSortie($sortie, $this->getUser()))
+        {
+            /* Si l'utilisateur veut ajouter un lieu on lui mets le formulaire de creation de lieu
+                       tout en supprimant de l'objet transféré le lieu selectionner afin que les champs du nouveau formulaire soient vides
+                    */
+            if(isset($request->get('sortie')['AjouterLieu']))
+            {
+
+                $form = $this->createForm(SortieType::class, $sortie, [
+                    'addLieu' => isset($request->get('sortie')['AjouterLieu']),
+                    'InsererLieu' => isset($request->get('sortie')['InsererLieu'])
+                ]);
+
+                $form->get('lieu')->setData(null);
+
+                $form->handleRequest($request);
+
+                return $this->render('sortie/creer.html.twig', [
+                    'form' => $form
+                ]);
+            }
+
+
+            $form = $this->createForm(SortieType::class, $sortie, [
+                'addLieu' => isset($request->get('sortie')['AjouterLieu']),
+                'InsererLieu' => isset($request->get('sortie')['InsererLieu'])
+            ]);
+
+            // On resupprime le lieu de la liste déroulante afin de laisser place au nouveau afin de faire une insertion
+            if(isset($request->get('sortie')['InsererLieu']))
+            {
+                $form->get('lieu')->setData(null);
+            }
+
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid())
+            {
+                $em->beginTransaction();
+                try {
+
+                    //dd($sortie->getLieu());
+                    // On persiste le lieu
+                    $em->persist($sortie->getLieu());
+                    $em->flush();
+
+                    // On persiste la sortie
+                    $em->persist($sortie);
+                    $em->flush();
+
+                    $em->commit();
+                } catch (\Exception $e) {
+                    $em->rollBack();
+                    var_dump($e->getMessage());
+                    return $this->render('sortie/update.html.twig', [
+                        'form' => $form
+                    ]);
+                }
+
+                $this->addFlash('success', 'Modification effectuée');
+                return $this->redirectToRoute('app_sorties_par_sites', ['site' => $this->getUser()->getSite()->getNom()]);
+            }
+
+
+            return $this->render('sortie/update.html.twig', [
+                'form' => $form
+            ]);
+        }
+
+        $this->addFlash('error', 'Vous ne pouvez pas modifier une sortie deja publiée');
+        return $this->redirectToRoute('app_sorties_par_sites', ['site' => $this->getUser()->getSite()->getNom()]);
+
+    }
+
+    #[Route('/sortie/cancel/{id}', name: 'app_sortie_cancel', requirements: ['id' => '\d+'])]
+    public function cancelSortie(EntityManagerInterface $em, Sortie $sortie, EtatRepository $etat, SortiesService $sortiesService) : Response
+    {
+
+        if($sortiesService->verifAnnulerSortie($sortie, $this->getUser()))
+        {
+            //dd($sortie);
+            $sortie->setEtat($etat->find(6));
+
+            $em->persist($sortie);
+            $em->flush();
+
+            return $this->redirectToRoute('app_sorties_par_sites', ['site' => $this->getUser()->getSite()->getNom()]);
+        }
+
+        $this->addFlash('error', 'Impossible d\'annuler une sortie qui n\'est pas encore ouverte ou qui a deja débuté');
+        return $this->render('sortie/detail.html.twig');
+
+    }
+
+    #[Route('/sortie/delete/{id}', name: 'app_sorties_delete', requirements: ['id' => '\d+'])]
+    public function deleteSortie(EntityManagerInterface $em, Sortie $sortie, EtatRepository $etat, SortiesService $sortiesService) : Response
+    {
+
+        if($sortiesService->verifSuppressionSortie($sortie, $this->getUser()))
+        {
+            $em->beginTransaction();
+            try {
+
+                $em->remove($sortie);
+                $em->flush();
+
+                $em->commit();
+            } catch (\Exception $e) {
+                $em->rollBack();
+                var_dump($e->getMessage());
+            }
+            return $this->redirectToRoute('app_sorties_par_sites', ['site' => $this->getUser()->getSite()->getNom()]);
+        }
+
+        $this->addFlash('error', 'Impossible de supprimer une sortie publiée');
+        return $this->redirectToRoute('app_sorties_par_sites', ['site' => $this->getUser()->getSite()->getNom()]);
+
+
+
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(SortieRepository $sortieRepository,SiteRepository $siteRepository): Response
     {
@@ -77,50 +268,6 @@ class SortieController extends AbstractController
             'sorties' => $sorties,
             'sites' => $sites,
         ]);
-    }
-
-    #[Route('/sortie/creer', name:'app_sortie_create')]
-    public function creerSortie(EntityManagerInterface $em, Request $request, ParticipantRepository $p, EtatRepository $e) : Response
-    {
-
-        $sortie = new Sortie();
-
-        $form = $this->createForm(SortieType::class, $sortie);
-
-        $form->handleRequest($request);
-
-
-
-        if($form->isSubmitted() && $form->isValid())
-        {
-
-
-            $em->beginTransaction();
-            try {
-                //$this->getUser()->getId();
-                $sortie->setOrganisateur($p->find(1));
-                $sortie->setEtat($e->find(1));
-                $em->persist($sortie->getLieu());
-                $em->flush();
-                $em->persist($sortie);
-                $em->flush();
-
-                $em->commit();
-            } catch (\Exception $e) {
-                $em->rollBack();
-                // Gérez l'exception
-            }
-
-
-
-            return $this->redirectToRoute('app_home');
-        }
-
-
-        return $this->render('sortie/creer.html.twig', [
-            'form' => $form
-        ]);
-
     }
 
 }
--- src/Form/LieuType.php
+++ src/Form/LieuType.php
@@ -4,10 +4,13 @@ namespace App\Form;
 
 use App\Entity\Lieu;
 use App\Entity\Ville;
+use App\Repository\SerieRepository;
+use App\Repository\VilleRepository;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -38,14 +41,14 @@ class LieuType extends AbstractType
                 'choice_label' => 'nom',
                 'placeholder' => 'Choisir une ville',
                 'required' => 'true',
-                'attr' => ['class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']
-            ])
-            ->add('actif', CheckboxType::class, [
-                'required' => false,
-                'attr' => ['class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']
-            ])
+                'attr' => ['class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'],
+                'query_builder' => function(VilleRepository $villeRepository) {
+                    return $villeRepository->createQueryBuilder('v');
+                },
+              ])
         ;
     }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([

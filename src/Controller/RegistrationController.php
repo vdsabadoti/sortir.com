8af83d7f3352b1cac1d92363d2cf7a80d5b68e37
marius.clement2023 <mariusclement68@gmail.com>
2024-02-27 16:31:57 +0100

    update composer

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -62,6 +62,7 @@ class RegistrationController extends AbstractController
     }
 
     #[Route('/registerByExcel', name: 'app_register_by_excel')]
+    #[IsGranted('ROLE_ADMIN')]
     public function registerByExcel(Request $request, RegistrationService $registrationService,EntityManagerInterface $entityManager) : Response
     {
 
